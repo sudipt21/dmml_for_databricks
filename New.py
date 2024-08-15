26c578864404eb9f02ccd64ab6add9da3ecf9fc9
sudipt.nigam@gmail.com <sudipt.nigam@gmail.com>
2024-08-15 18:32:32 +0000

    fib

--- New.py
+++ New.py
@@ -1,2 +1,20 @@
 # Databricks notebook source
+def fibonacci_sum(n):
+    if n <= 0:
+        return 0
+    fibo = [0, 1]
+    sum = fibo[0] + fibo[1]
+    
+    while True:
+        next_fibo = fibo[-1] + fibo[-2]
+        if next_fibo > n:
+            break
+        fibo.append(next_fibo)
+        sum += next_fibo
+    
+    return sum
 
+# Example usage
+n = 10
+result = fibonacci_sum(n)
+display(result)
